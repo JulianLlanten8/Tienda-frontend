86f673199aea3d95b67a1f4b10733bf2022428ee
JulianLlanten8 <hugojulians@gmail.com>
2022-04-24 13:49:37 -0500

    frontend Ok ✅

--- deploy.sh
+++ deploy.sh
@@ -9,16 +9,10 @@ npm run build
 # navigate into the build output directory
 cd dist
 
-# if you are deploying to a custom domain
-# echo 'www.example.com' > CNAME
-
 git init
 git add -A
 git commit -m 'deploy'
 
-# if you are deploying to https://<USERNAME>.github.io
 git push -f git@github.com:https://github.com/JulianLlanten8/Tienda-frontend/.github.io.git main
 
-# if you are deploying to https://<USERNAME>.github.io/<REPO>
-# git push -f git@github.com:<USERNAME>/<REPO>.git main:gh-pages
 cd -
